7390d9f490adf112ca37608eab51fd3ce7abac22
matedavid <david.mate.lopez@gmail.com>
2020-12-14 01:46:48 +0100

    reading sisa file

--- src/main.c
+++ src/main.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void) {
-  printf("Hello, World!\n");
+int main(int argc, char **argv) {
+  if (argc <= 1) {
+    printf("You need to specify a path to the file\n");
+    return -1;
+  }
+
+  FILE *fp;
+  fp = fopen(argv[1], "r");
+
+  char buff[256];
+  while (fgets(buff, 256, fp)) {
+    printf("%s", buff);
+  }
 }
